b9b7636407049ffaea7da46508b7979ed41c1abb
filipesaretta <filipessm124@gmail.com>
2022-04-15 16:01:26 -0300

    Add new section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Features } from './components/Features';
 import { MainLanding } from './components/MainLanding';
+import { PopularOverWeek } from './components/PopularOverWeek';
 
 function App() {
   return (
     <>
       <MainLanding />
       <Features />
+      <PopularOverWeek />
     </>
   );
 }
---
+++ src/components/PopularOverWeek/index.tsx
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+
+import person1 from '../../assets/astronauta 1.png';
+import person2 from '../../assets/astronauta-2.png';
+import person3 from '../../assets/astronauta-3.png';
+import person4 from '../../assets/astronauta-4.png';
+import arrow from '../../assets/arrow-white.svg';
+
+const Container = styled.div`
+  max-width: 1270px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(2, max-content);
+  justify-content: space-between;
+  padding: 0 2rem;
+  padding-bottom: 5rem;
+  h3 {
+    grid-column: 1 / -1;
+    font-size: 4rem;
+    padding: 10rem 0;
+    span {
+      color: var(--color-logo);
+    }
+  }
+`;
+const Astronaut = styled.a`
+  p {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 2rem;
+    padding-bottom: 1rem;
+    color: var(--subtitle-color);
+    font-size: 2rem;
+    span {
+      color: var(--color-logo);
+    }
+  }
+
+  p:first-child {
+    color: white;
+    font-size: 3rem;
+  }
+`;
+
+export function PopularOverWeek() {
+  return (
+    <Container>
+      <h3>
+        <span>Populares</span> da Semana
+      </h3>
+      <Astronaut>
+        <p>
+          Astronauta 1 <img src={arrow} alt="" />
+        </p>
+        <p>
+          Jonh Doe <span> 1.50RKT</span>
+        </p>
+        <img src={person1} alt="" />
+      </Astronaut>
+      <Astronaut>
+        <p>
+          Astronauta 2 <img src={arrow} alt="" />
+        </p>
+        <p>
+          Jane Doe <span> 4.50RKT</span>
+        </p>
+        <img src={person2} alt="" />
+      </Astronaut>
+      <Astronaut>
+        <p>
+          Astronauta 3 <img src={arrow} alt="" />
+        </p>
+        <p>
+          Maria Luigia <span> 7.50RKT</span>
+        </p>
+        <img src={person3} alt="" />
+      </Astronaut>
+      <Astronaut>
+        <p>
+          Astronauta 4 <img src={arrow} alt="" />
+        </p>
+        <p>
+          Giuseppe Rossi <span> 5.50RKT</span>
+        </p>
+        <img src={person4} alt="" />
+      </Astronaut>
+    </Container>
+  );
+}
